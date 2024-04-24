66de822887643d1c205f644c26c7bf675b2719e2
Rishabh Garcha <rish889@gmail.com>
2024-04-24 18:56:56 +0530

    Health Check

--- src/main/kotlin/com/epam/kotlinSpringOrderService/controller/HealthController.kt
+++ src/main/kotlin/com/epam/kotlinSpringOrderService/controller/HealthController.kt
@@ -1,4 +1,4 @@
-package com.epam.kotlinSpringOrderService
+package com.epam.kotlinSpringOrderService.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
