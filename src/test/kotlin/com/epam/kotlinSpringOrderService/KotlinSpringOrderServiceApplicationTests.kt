e57baf3cd40712a1be0f74e2e6707fe882d514d2
Rishabh Garcha <rish889@gmail.com>
2024-04-24 18:19:28 +0530

    Initial Commit

---
+++ src/test/kotlin/com/epam/kotlinSpringOrderService/KotlinSpringOrderServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.epam.kotlinSpringOrderService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringOrderServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
