0ab332b89817eabd91cb23f42988bec2b48b6bc9
Rishabh Garcha <rish889@gmail.com>
2024-04-25 11:20:00 +0530

    Health Check

--- src/test/kotlin/com/epam/kotlinSpringOrderService/controller/HealthControllerWebMvcTest.kt
+++ src/test/kotlin/com/epam/kotlinSpringOrderService/controller/HealthControllerWebMvcTest.kt
@@ -1,13 +1,10 @@
 package com.epam.kotlinSpringOrderService.controller
 
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
